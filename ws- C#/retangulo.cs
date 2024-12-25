5c1f0098e0c0c3d85301d2d848bf0eb82e9797ff
Rhuan Santos <rhuankito@hotmail.com>
2024-12-25 17:05:47 -0300

    Add files via upload

---
+++ ws- C#/retangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Programa
+{
+    class retangulo
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double bas, altura, area, perimetro, diagonal; 
+
+            Console.Write("Base do retangulo: ");
+            bas = double.Parse(Console.ReadLine(), CI);
+            Console.Write("Altura do retangulo: ");
+            altura = double.Parse(Console.ReadLine(), CI);
+
+            area = bas * altura;
+            perimetro = 2 * ( bas + altura);
+            diagonal = Math.Sqrt(Math.Pow(bas, 2.0) + Math.Pow(altura, 2.0));
+
+            Console.WriteLine("AREA = " + area.ToString("F4", CI));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", CI));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CI));
+        }
+    }
+}
