cc6707bec0f743f210dce42cb83fcf6f6cc1f53a
Rhuan Santos <rhuankito@hotmail.com>
2024-12-25 17:53:35 -0300

    Add files via upload

---
+++ ws- C#/Hora.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hora
+{
+    class Hora
+    {
+        static void Main(string[] args)
+        {
+            int hora;
+
+            Console.Write("Digite uma hora do dia: ");
+            hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (hora < 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite!");
+            }
+        }
+    }
+}
