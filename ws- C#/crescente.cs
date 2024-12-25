305d61c363a24a553b1f1eb26d49a4048178c38f
Rhuan Santos <rhuankito@hotmail.com>
2024-12-25 18:21:39 -0300

    Add files via upload

---
+++ ws- C#/crescente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Programa
+{
+    class crescente
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Digite dois numeros:");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            while(a != b)
+            {
+                if(a > b)
+                {
+                    Console.WriteLine("DECRESCENTE!");
+                }
+                else
+                {
+                    Console.WriteLine("CRESCENTE!");
+                }
+                Console.WriteLine("Digite dois numeros:");
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
